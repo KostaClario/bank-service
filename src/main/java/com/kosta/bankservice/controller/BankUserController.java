014c4bbb8cafcce5312877198e474ad9d5ab178e
taebin5824 <xoqls5824@gmail.com>
2025-07-10 00:57:56 +0900

    feat: 사용자 정보 반환 컨트롤러(DB구현예정)

---
+++ src/main/java/com/kosta/bankservice/controller/BankUserController.java
@@ -0,0 +1,28 @@
+package com.kosta.bankservice.controller;
+
+
+import com.kosta.bankservice.dto.BankUserInfoRequest;
+import com.kosta.bankservice.dto.BankUserInfoResponse;
+import com.kosta.bankservice.util.CiGenerator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bank")
+public class BankUserController {
+
+    @PostMapping("/user-info")
+    public BankUserInfoResponse getUserInfo(@RequestBody BankUserInfoRequest request) {
+        String ci = CiGenerator.generateCi(request.getEmail(), request.getPhone());
+
+        return BankUserInfoResponse.builder()
+                .name("임태빈")
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .ci(ci)
+                .bankAccount("110-1111-2222")
+                .build();
+    }
+}
