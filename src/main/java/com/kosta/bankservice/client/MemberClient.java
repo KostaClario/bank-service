2065f4b0acdfa14968fd602b4c78639a5e5f8fcc
taebin5824 <xoqls5824@gmail.com>
2025-07-13 21:38:42 +0900

    feat: user-service에 거래내역 전송

---
+++ src/main/java/com/kosta/bankservice/client/MemberClient.java
@@ -0,0 +1,14 @@
+package com.kosta.bankservice.client;
+
+import com.kosta.bankservice.dto.BankApiDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "user-service")
+public interface MemberClient {
+    @PostMapping("/internal/transactions")
+    void sendTransactions(@RequestBody List<BankApiDTO> transactions);
+}
