0701c05c01d6c935585405b20ef56087998f3567
taebin5824 <xoqls5824@gmail.com>
2025-07-13 21:39:45 +0900

    refactor: Eureka 클라이언트 어노테이션 추가

--- src/main/java/com/kosta/bankservice/BankServiceApplication.java
+++ src/main/java/com/kosta/bankservice/BankServiceApplication.java
@@ -2,8 +2,12 @@ package com.kosta.bankservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class BankServiceApplication {
 
     public static void main(String[] args) {
